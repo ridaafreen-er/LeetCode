9b0779cf3984e54b80022f3315a74d6135604140
Rida Aafreen <aafreenridamf@yahoo.com>
2026-09-18 09:14:08 +0530

    Time: 0 ms (100%), Space: 20.3 MB (41.63%) - LeetHub

---
+++ 0230-kth-smallest-element-in-a-bst/0230-kth-smallest-element-in-a-bst.py
@@ -0,0 +1,18 @@
+class Solution(object):
+    def kthSmallest(self, root, k):
+        stack = []
+        current = root
+
+        while True:
+            while current:
+                stack.append(current)
+                current = current.left
+
+            current = stack.pop()
+
+            k -= 1
+
+            if k == 0:
+                return current.val
+
+            current = current.right
