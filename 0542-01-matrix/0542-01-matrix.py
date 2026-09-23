dca3e469a1bde0873130f3a1738a84f09d052859
Rida Aafreen <aafreenridamf@yahoo.com>
2026-09-23 23:29:39 +0530

    Time: 155 ms (84.07%), Space: 14.9 MB (74.07%) - LeetHub

---
+++ 0542-01-matrix/0542-01-matrix.py
@@ -0,0 +1,43 @@
+from collections import deque
+
+class Solution(object):
+    def updateMatrix(self, mat):
+
+        rows = len(mat)
+        cols = len(mat[0])
+
+        queue = deque()
+
+        for r in range(rows):
+            for c in range(cols):
+
+                if mat[r][c] == 0:
+                    queue.append((r, c))
+
+                else:
+                    mat[r][c] = -1
+
+        directions = [
+            (1, 0),
+            (-1, 0),
+            (0, 1),
+            (0, -1)
+        ]
+
+        while queue:
+
+            r, c = queue.popleft()
+
+            for dr, dc in directions:
+
+                nr = r + dr
+                nc = c + dc
+
+                if 0 <= nr < rows and 0 <= nc < cols:
+
+                    if mat[nr][nc] == -1:
+
+                        mat[nr][nc] = mat[r][c] + 1
+                        queue.append((nr, nc))
+
+        return mat
